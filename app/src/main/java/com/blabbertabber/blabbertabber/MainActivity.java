87d59121e510c5f6c7e35365614d6058369d11c3
Brian Cunnie <brian.cunnie@gmail.com>
2015-08-22 08:10:43 -0700

    Just got first UI control test working.

--- app/src/main/java/com/blabbertabber/blabbertabber/MainActivity.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button_record);
     }
 
     @Override
--- app/src/main/java/com/blabbertabber/blabbertabber/tests/MainActivityTest.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/tests/MainActivityTest.java
@@ -1,19 +1,24 @@
 package com.blabbertabber.blabbertabber.tests;
 
-import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.ViewAsserts;
+import android.test.suitebuilder.annotation.SmallTest;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 import com.blabbertabber.blabbertabber.MainActivity;
-
-import junit.framework.TestCase;
+import com.blabbertabber.blabbertabber.R;
 
 /**
- * Created by cunnie on 8/9/15.
- * Tests MainActivity by simulating a click on the record button.
+ * Created by cunnie on 8/22/15.
+ * Tests the Main activity by confirming presence and functionality of record_button.
  */
+//public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+//}
+
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     private MainActivity mMainActivity;
@@ -26,26 +31,39 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-//        setActivityInitialTouchMode(true);
-//
-//        mMainActivity = getActivity();
-//        mRecordingButton = (Button)
-//                mMainActivity.findViewById(R.id.button_record);
+        setActivityInitialTouchMode(true);
 
+        mMainActivity = getActivity();
+        mRecordingButton = mMainActivity.findViewById(R.id.button_record);
+
+    }
+
+    @SmallTest
+    public void testClickMeButton_layout() {
+        final View decorView = mMainActivity.getWindow().getDecorView();
+
+        ViewAsserts.assertOnScreen(decorView, mRecordingButton);
+
+        final ViewGroup.LayoutParams layoutParams =
+                mRecordingButton.getLayoutParams();
+        assertNotNull(layoutParams);
+        assertEquals(layoutParams.width, 200);
+        assertEquals(layoutParams.height, 200);
     }
 
     public void tearDown() throws Exception {
+        super.tearDown();
     }
 
-    public void testOnCreate() throws Exception {
+//    public void testOnCreate() throws Exception {
+//        super.testOnCreate();
+//    }
 
-    }
-
-    public void testOnCreateOptionsMenu() throws Exception {
-
-    }
-
-    public void testOnOptionsItemSelected() throws Exception {
-
-    }
+//    public void testOnCreateOptionsMenu() throws Exception {
+//        super.testOnCreateOptionsMenu();
+//    }
+//
+//    public void testOnOptionsItemSelected() throws Exception {
+//
+//    }
 }
