16bab1e664b28f5cc1bb4758b02d31f25c0942c9
Brendan Cunnie <saintbrendan@gmail.com>
2015-09-12 10:07:22 -0700

    Initial Test for RecordingActivity
    Signed-off-by: Brian Cunnie <brian.cunnie@gmail.com>

---
+++ app/src/androidTest/java/com/blabbertabber/blabbertabber/RecordingActivityTest.java
@@ -0,0 +1,29 @@
+package com.blabbertabber.blabbertabber;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by cunnie on 9/12/15.
+ * Test Recording Activity
+ */
+public class RecordingActivityTest {
+    @Rule
+    public IntentsTestRule<RecordingActivity> mActivityRule =
+            new IntentsTestRule<RecordingActivity>(RecordingActivity.class);
+
+    @Test
+    public void recordingButtonTest() {
+        // fail: android:visibility="gone" in activity_recording.xml
+        onView(withId(R.id.recording_root_view)).check(matches(isDisplayed()));
+    }
+
+}
--- app/src/main/java/com/blabbertabber/blabbertabber/RecordingActivity.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/RecordingActivity.java
@@ -1,6 +1,7 @@
 package com.blabbertabber.blabbertabber;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by cunnie on 8/16/15.
@@ -8,4 +9,10 @@ import android.app.Activity;
  *
  */
 public class RecordingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording);
+    }
+
 }
