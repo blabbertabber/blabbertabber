723ada29d04c4006ee56efc91c6891ec2127e780
Brendan Cunnie <saintbrendan@gmail.com>
2015-09-13 16:35:44 -0700

    Service created AND runs!
    Signed-off-by: Brian Cunnie <brian.cunnie@gmail.com>

--- app/src/main/java/com/blabbertabber/blabbertabber/RecordingActivity.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/RecordingActivity.java
@@ -6,38 +6,73 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by cunnie on 8/16/15.
  * Activity to record and identify voices.
  */
 public class RecordingActivity extends Activity {
+    RecordingService mService;
+    boolean mBound = false;
 
     private static final String TAG = "RecordingActivity";
     protected ServiceConnection mServerConn = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder binder) {
-            Log.wtf(TAG, "onServiceConnected");
+            RecordingService.RecordingBinder recordingBinder = (RecordingService.RecordingBinder) binder;
+            mService = recordingBinder.getService();
+            mBound = true;
+            Log.wtf(TAG, "onServiceConnected.  mBound: " + mBound);
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
             Log.wtf(TAG, "onServiceDisconnected");
         }
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recording);
-
-
+    protected void onStart() {
+        super.onStart();
         Intent serviceIntent = new Intent(this, RecordingService.class);
         if (bindService(serviceIntent, mServerConn, BIND_AUTO_CREATE)) {
-            Log.wtf(TAG, "bindService() succeeded");
+            Log.wtf(TAG, "bindService() succeeded, mBound: " + mBound);
         } else {
-            Log.wtf(TAG, "bindService() failed");
+            Log.wtf(TAG, "bindService() failed, mBound: " + mBound);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_recording);
+
+        int id = 15;
+        if (mBound) {
+            id = mService.getSpeakerId();
+        }
+        Toast.makeText(getApplicationContext(), "" + id + "  mBound: " + mBound, Toast.LENGTH_LONG).show();
+    }
+
+    public void displaySpeakerId(View v) {
+        int id = 17;
+        if (mBound) {
+            id = mService.getSpeakerId();
+        }
+        Toast.makeText(getApplicationContext(), "" + id + "  mBound: " + mBound, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();;
+
+        if (mServerConn != null) {
+            unbindService(mServerConn);
         }
     }
 
--- app/src/main/java/com/blabbertabber/blabbertabber/RecordingService.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/RecordingService.java
@@ -2,6 +2,7 @@ package com.blabbertabber.blabbertabber;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Messenger;
@@ -10,6 +11,13 @@ import android.util.Log;
 public class RecordingService extends Service {
     private static final String TAG = "RecordingService";
     final Messenger myMessenger = new Messenger(new Handler());
+    private final IBinder mBinder = new RecordingBinder();
+
+    public class RecordingBinder extends Binder {
+        RecordingService getService() {
+            return RecordingService.this;
+        }
+    }
 
     @Override
     public void onCreate() {
@@ -23,6 +31,14 @@ public class RecordingService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
 //        throw new UnsupportedOperationException("Not yet implemented");
-        return myMessenger.getBinder();
+        return mBinder;
     };
+
+    public int getSpeakerId() {
+        return 2;
+    }
+
+    public int getSpeakerVolume() {
+        return 3;
+    }
 }
