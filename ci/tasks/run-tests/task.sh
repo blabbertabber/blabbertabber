a8736f5f5d3371bca0951d3d2383d7ef7bb5faa1
Brian Cunnie <brian.cunnie@gmail.com>
2015-10-14 17:50:29 -0700

    Beginnings of Android Test Run

--- ci/tasks/run-tests/task.sh
+++ ci/tasks/run-tests/task.sh
@@ -2,4 +2,12 @@
 
 set -eux
 
-echo "I love my dog"
+pushd blabbertabber/
+ls -ld /proc/kvm
+
+echo no | android create avd --force -n test -t android-23 --abi armeabi-v7a
+emulator -avd test -no-skin -no-audio -no-window &
+sleep 90
+adb shell input keyevent 82 &
+android list target
+./gradlew connectedAndroidTest -PdisablePreDex
