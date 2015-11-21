06044eddea08d03bd6ccbaac28d7c54db6ece9f9
Brian Cunnie <brian.cunnie@gmail.com>
2015-11-21 12:57:39 -0800

    Bars and durations; Bars don't work, durations do.

--- app/src/main/java/com/blabbertabber/blabbertabber/RectangleView.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/RectangleView.java
@@ -14,35 +14,54 @@ import android.view.View;
  */
 public class RectangleView extends View {
     private static final String TAG = "RectangleView";
-
     View speakerBar0;
+    private boolean mVisible = true;
+    private int mColor = Color.GREEN;
+    private float mBarRatio = (float) 0.50;
 
     public RectangleView(Context context, AttributeSet attrs) {
         super(context);
-        Log.i(TAG, "started constructor -------------------------------======");
+        Log.i(TAG, "RectangleView()");
     }
 
     public RectangleView(Context context) {
         super(context);
-        Log.i(TAG, "started constructor -------------------------------");
-        speakerBar0 = findViewById(R.id.bar_speaker_0);
+        Log.i(TAG, "RectangleView()");
+    }
+
+    public void setColor(int color) {
+        mColor = color;
+    }
+
+    public void setVisible(boolean visible) {
+        mVisible = visible;
+    }
+
+    public void setBarRatio(float barRatio) {
+        // mBarRatio can range from 0 to 1.0
+        // 1.0 means it's the longest speaker
+        // 0.5 means that this speaker has spoken half as much as the longest speaker
+        // etc...
+        mBarRatio = barRatio;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Log.i(TAG, "Just started onDraw() -------------------------------");
+        Log.i(TAG, "onDraw()");
 
-        RectF rect = new RectF();
-        rect.left = getLeft();
-        rect.top = getTop();
-        rect.right = getRight();
-        rect.bottom = getBottom();
+        if (mVisible) {
+            RectF rect = new RectF();
+            rect.left = 0;
+            rect.top = 0;
+            rect.right = mBarRatio * (float) getWidth();
+            Log.wtf(TAG, "getWidth() " + getWidth() + " rect.right " + rect.right);
+            rect.bottom = getHeight();
 
-        Paint myPaint = new Paint();
-        myPaint.setColor(Color.RED);
-        myPaint.setStrokeWidth(3);
-        canvas.drawRect(rect, myPaint);
-        Log.i(TAG, "Just finishing onDraw() -------------------------------" + rect.left + " " + rect.top + " " + rect.right + " " + rect.bottom);
+            Paint myPaint = new Paint();
+            myPaint.setColor(mColor);
+            myPaint.setStrokeWidth(1);
+            canvas.drawRect(rect, myPaint);
+        }
     }
 }
