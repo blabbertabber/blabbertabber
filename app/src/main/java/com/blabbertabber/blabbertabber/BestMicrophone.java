7be152f483ddb696487a9dd5a44f938b4a9393c0
Brian Cunnie <brian.cunnie@gmail.com>
2015-11-24 07:33:38 -0800

    BestMicrophone uses static methods
    - no constructor(s)
    - default behavior: returns DEFAULT microphone instead of MIC
    - changes to BestMicrophoneTest
      - assertion arguments are now in proper order (expected, actual)
      - BestMicrophone is called using static methods
      - test is updated to reflect new default behavior
    - comments explained why we didn't make BuildMODEL's methods static.
    - explained RecordingActivityTest failure the first time the app is
      installed (permission dialog)

--- app/src/main/java/com/blabbertabber/blabbertabber/BestMicrophone.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/BestMicrophone.java
@@ -11,17 +11,6 @@ import android.util.Log;
 
 public class BestMicrophone {
     private static final String TAG = "BestMicrophone()";
-    private BuildMODEL mBuildModel;
-
-    public BestMicrophone() {
-        this(new BuildMODEL());
-    }
-
-    // use constructor injection to test with Mockito mocks.
-    public BestMicrophone(BuildMODEL b) {
-        mBuildModel = b;
-    }
-
     // Different mics work better for different models
     // e.g. Nexus 5 == MIC; Nexus 6 == VOICE_RECOGNITION
     // http://stackoverflow.com/questions/1995439/get-android-phone-model-programmatically
@@ -38,20 +27,24 @@ public class BestMicrophone {
     //                    VOICE_CALL
     //                    VOICE_DOWNLINK
 
-    public int getBestMicrophone() {
-        String model = mBuildModel.model();
+    public static int getBestMicrophone() {
+        return getBestMicrophone(new BuildMODEL());
+    }
+
+    // method
+    public static int getBestMicrophone(BuildMODEL buildModel) {
+        String model = buildModel.model();
         Log.v(TAG, "getBestMicrophone() Build.MODEL == " + model);
         switch (model) {
             case "Nexus 4":
             case "Nexus 5":
                 return (MediaRecorder.AudioSource.MIC);
-            case "Nexus 5X":
-                return (MediaRecorder.AudioSource.DEFAULT);
             case "Nexus 6":
                 return (MediaRecorder.AudioSource.VOICE_RECOGNITION);
+            case "Nexus 5X":
             default:
                 Log.v(TAG, "getBestMicrophone() FIXME new model: " + model);
-                return (MediaRecorder.AudioSource.MIC);
+                return (MediaRecorder.AudioSource.DEFAULT);
         }
 
     }
