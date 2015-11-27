9aadd784a370713535826196651a79a4c36e2045
Brendan Cunnie <saintbrendan@gmail.com>
2015-11-27 13:54:43 -0500

    Create destination subdirectory and file for sound data.
    - removed now-unneeded EmulatorRecorder (apparently emulator DOES do sound)
    - added SDK 23 callback permissions to write external storage
    - created BlabberTabber directory to not write in root (no need for sloppiness)
    - added the LIUM .jar file, but not using it yet
    
    Signed-off-by: Brian Cunnie <brian.cunnie@gmail.com>

--- app/src/main/java/com/blabbertabber/blabbertabber/RecordingService.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/RecordingService.java
@@ -3,7 +3,6 @@ package com.blabbertabber.blabbertabber;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
-import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -12,8 +11,7 @@ public class RecordingService extends Service {
     private static final String TAG = "RecordingService";
     private final IBinder mBinder = new RecordingBinder();
     private Thread mThreadRecorder;
-    // emulator crashes if attempts to use the actual microphone, so we simulate microphone in EmulatorRecorder
-    private Recorder mRecorder = "goldfish".equals(Build.HARDWARE) ? new EmulatorRecorder(this) : new DeviceRecorder(this);
+    private Recorder mRecorder = new DeviceRecorder(this);
 
     public RecordingService() {
     }
