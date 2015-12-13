88cfaa83fb3111e02e4b035f443cb9f7d1697d98
Brian Cunnie <brian.cunnie@gmail.com>
2015-12-12 17:45:54 -0800

    User can playback meeting
    - Also can display splash screen again
    - Rush Limbaugh Easter Egg

--- app/src/main/java/com/blabbertabber/blabbertabber/MainActivity.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/MainActivity.java
@@ -4,16 +4,20 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends Activity {
+import java.io.File;
 
+public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
     private boolean mFirstTime = true;
+    public static boolean resetFirstTime = false;
+    private int rushLimbaughIsWrongCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +30,12 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        Log.i(TAG, "onResume()");
         // http://developer.android.com/training/basics/data-storage/shared-preferences.html
         SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
         mFirstTime = sharedPref.getBoolean(getString(R.string.first_time), mFirstTime);
 
-        if (!mFirstTime) {
+        if (!mFirstTime && !resetFirstTime) {
             launchRecordingActivity();
         }
     }
@@ -47,6 +52,7 @@ public class MainActivity extends Activity {
 
     public void launchRecordingActivity() {
         mFirstTime = false;
+        resetFirstTime = false;
 
         Intent intent = new Intent(this, RecordingActivity.class);
         startActivity(intent);
@@ -56,6 +62,22 @@ public class MainActivity extends Activity {
     // requirement to pass in a View (which is never used)
     public void launchRecordingActivity(View view) {
         launchRecordingActivity();
+    }
+
+    // Easter Egg for new users
+    public void rushLimbaughIsWrong(View v) {
+        Log.i(TAG, "rushLimbaughIsWrong()");
+        rushLimbaughIsWrongCount += 1;
+        if (rushLimbaughIsWrongCount > 3) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.google.com/search?q=rush+limbaugh+wrong"));
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                Log.v(TAG, "rushLimbaughIsWrong(): resolved activity");
+                startActivity(intent);
+            } else {
+                Log.v(TAG, "rushLimbaughIsWrong(): couldn't resolve activity");
+            }
+        }
     }
 
     // needed for testing
