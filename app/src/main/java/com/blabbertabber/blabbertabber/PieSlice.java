fa01871d4a98324b06bbc9b16795a5f9f5920ce0
Brian Cunnie <brian.cunnie@gmail.com>
2015-12-17 04:31:28 -0800

    RecordingActivity has 3 pie slices: Red, Yellow, Blue
    - PieSlice is a View
    - PieSlice has custom attributes that can be set in layout XML
      - color
      - startAngle
      - sweepAngle

--- app/src/main/java/com/blabbertabber/blabbertabber/PieSlice.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/PieSlice.java
@@ -1,11 +1,13 @@
 package com.blabbertabber.blabbertabber;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -14,20 +16,33 @@ import android.view.View;
 public class PieSlice extends View {
     private static final String TAG = "PieSlice";
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private double[] value_degree;
     private int color = Color.YELLOW;
+    private float startAngle = 0;
+    private float sweepAngle = 45;
 
     public PieSlice(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs);
     }
 
     public PieSlice(Context context) {
         super(context);
     }
 
+    private void init(Context context, AttributeSet attrs){
+        // http://stackoverflow.com/questions/8302229/accessing-attrs-in-attributeset-for-custom-components
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.PieSlice);
+        color = a.getColor(R.styleable.PieSlice_pieColor, Color.BLACK);
+        startAngle = a.getFloat(R.styleable.PieSlice_startAngle, 0);
+        sweepAngle = a.getFloat(R.styleable.PieSlice_sweepAngle, 0);
+        a.recycle();
+        Log.i(TAG, "init() color: " + color + " startAngle " + startAngle + " sweepAngle " + sweepAngle);
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        Log.i(TAG, "onDraw() color: " + color + " startAngle " + startAngle + " sweepAngle " + sweepAngle);
 
         int width = getWidth();
         int height = getHeight();
@@ -37,8 +52,6 @@ public class PieSlice extends View {
         RectF rectf = new RectF(leftPad, topPad, squareLength + leftPad, squareLength + topPad);
 
         paint.setColor(color);
-        float startAngle = 0;
-        float stopAngle = 45;
-        canvas.drawArc(rectf, startAngle, stopAngle, true, paint);
+        canvas.drawArc(rectf, startAngle, sweepAngle, true, paint);
     }
 }
