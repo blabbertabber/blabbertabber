9c282049801cb47d73c4ffeb389215a156a93763
Brian Cunnie <brian.cunnie@gmail.com>
2016-01-24 21:12:11 -0500

    Added AudioRecorReal and AudioRecordEmulator, and associated changes.
    Signed-off-by: Brendan Cunnie <saintbrendan@gmail.com>

--- app/src/main/java/com/blabbertabber/blabbertabber/AudioRecordAbstract.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/AudioRecordAbstract.java
@@ -5,13 +5,16 @@ package com.blabbertabber.blabbertabber;
  */
 public abstract class AudioRecordAbstract {
 
-
     public AudioRecordAbstract() {
     }
-
-    protected abstract void start();    // starts the recording, resumes recording
 
     protected abstract void stop();     // stops the recording, closes file, cannot resume after stop().
 
     public abstract void release();
+
+    public abstract void setRecordPositionUpdateListener(AudioEventProcessor audioEventProcessor);
+
+    public abstract int setPositionNotificationPeriod(int numFrames);
+
+    public abstract void startRecording();
 }
---
+++ app/src/main/java/com/blabbertabber/blabbertabber/AudioRecordEmulator.java
@@ -0,0 +1,55 @@
+package com.blabbertabber.blabbertabber;
+
+import android.util.Log;
+
+/**
+ * Created by brendancunnie on 1/24/16.
+ */
+public class AudioRecordEmulator extends AudioRecordAbstract {
+    static final String TAG = "AudioRecordEmulator";
+    private AudioEventProcessor audioEventProcessor;
+    Thread notifier;
+
+    public AudioRecordEmulator(int recorderAudioSource, int recorderSampleRateInHz, int recorderChannelConfig, int recorderAudioFormat, int recorderBufferSizeInBytes) {
+
+    }
+
+    @Override
+    protected void stop() {
+        notifier.interrupt();
+    }
+
+    @Override
+    public void release() {
+        notifier.interrupt();
+    }
+
+    @Override
+    public void setRecordPositionUpdateListener(AudioEventProcessor audioEventProcessor) {
+        this.audioEventProcessor = audioEventProcessor;
+    }
+
+    @Override
+    public int setPositionNotificationPeriod(int numFrames) {
+        return 0;
+    }
+
+    @Override
+    public void startRecording() {
+        /// start new thread
+        notifier = new Thread() {
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.currentThread().sleep(200);
+                        audioEventProcessor.onPeriodicNotificationEmulator();
+                    } catch (InterruptedException e) {
+                        Log.i(TAG, "run()   Huh.  InterruptedException thrown while sleep()ing.");
+                        e.printStackTrace();
+                        return;
+                    }
+                }
+            }
+        };
+    }
+}
