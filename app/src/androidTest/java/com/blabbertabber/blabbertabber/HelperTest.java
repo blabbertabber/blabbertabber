1a94f0746bc66f57d88a6aacbc56853835142859
Brendan Cunnie <saintbrendan@gmail.com>
2016-01-30 11:23:29 -0800

    Helper method to copy InputStream to file
    Signed-off-by: Brian Cunnie <brian.cunnie@gmail.com>

--- app/src/androidTest/java/com/blabbertabber/blabbertabber/HelperTest.java
+++ app/src/androidTest/java/com/blabbertabber/blabbertabber/HelperTest.java
@@ -2,8 +2,17 @@ package com.blabbertabber.blabbertabber;
 
 import android.support.test.runner.AndroidJUnit4;
 
+import org.hamcrest.core.StringContains;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -14,6 +23,8 @@ import static junit.framework.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class HelperTest {
     private static final String TAG = "HelperTest";
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
     @Test
     public void testHMSZero_0Duration() {
@@ -134,5 +145,41 @@ public class HelperTest {
     @Test
     public void testPoint1H_1Duration() {
         assertEquals("3_600_050ms should return '1:00:00.0'", "1:00:00.0", Helper.timeToHMMSSm(3_600_050));
+    }
+
+    // We only test error conditions; we cannot find a path that we can
+    // successfully write to in our test environment
+    // Once BlabberTabber has been run once
+    // we can write to "/data/user/0/com.blabbertabber.blabbertabber/files/sphinx"
+    @Test
+    public void testCopyRawToFilesystem00() throws Exception {
+        InputStream inputStream = new ByteArrayInputStream(new String("blah").getBytes(StandardCharsets.UTF_8));
+        File file = new File("/some/nonexistent/directory/sphinx");
+
+        exception.expect(IOException.class);
+        exception.expectMessage(StringContains.containsString("ENOENT (No such file or directory)"));
+        Helper.copyInputFileStreamToFilesystem(inputStream, file.getAbsolutePath());
+    }
+
+    @Test
+    public void testCopyRawToFilesystem01() throws Exception {
+        InputStream inputStream = new ByteArrayInputStream(new String("blah").getBytes(StandardCharsets.UTF_8));
+        File file = new File("sphinx");
+
+        exception.expect(IOException.class);
+        exception.expectMessage(StringContains.containsString("open failed: EROFS (Read-only file system)"));
+        Helper.copyInputFileStreamToFilesystem(inputStream, file.getAbsolutePath());
+        assertEquals("the file is created", file.exists(), true);
+    }
+
+    @Test
+    public void testCopyRawToFilesystem02() throws Exception {
+        InputStream inputStream = new ByteArrayInputStream(new String("blah").getBytes(StandardCharsets.UTF_8));
+        File file = new File("/data/local/tmp/sphinx");
+
+        exception.expect(IOException.class);
+        exception.expectMessage(StringContains.containsString("open failed: EACCES (Permission denied)"));
+        Helper.copyInputFileStreamToFilesystem(inputStream, file.getAbsolutePath());
+        assertEquals("the file is created", file.exists(), true);
     }
 }
