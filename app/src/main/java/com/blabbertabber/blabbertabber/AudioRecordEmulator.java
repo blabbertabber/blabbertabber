62d7aeb92de0ec0befadbc5713ce309651047afb
Brian Cunnie <brian.cunnie@gmail.com>
2016-02-15 09:21:04 -0800

    Enable BlabberTabber tests to run on the emulator
    - Real and emulated audio events are processed with the same logic.
      - emulated audio data is random (white noise)
    - TheSpeakers and TheSpeakersTest classes have been removed
      - replace by SpeakersBuilder
    - removed fake real-time diarization identification
      - variables
        - speaker
        - nextSpeakerChange
        - mPreviousSpeakerId
      - functions
        - getSpeakerId()
        - newSpeaker()
        - stopPreviousSpeaker()
    - UPDATES_PER_SECOND now a variable
    - WavFileTest uses correct TAG
    - meeting duration is calculated from size of raw audio file
    
    Signed-off-by: Brendan Cunnie <saintbrendan@gmail.com>

--- app/src/main/java/com/blabbertabber/blabbertabber/AudioRecordEmulator.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/AudioRecordEmulator.java
@@ -2,6 +2,8 @@ package com.blabbertabber.blabbertabber;
 
 import android.util.Log;
 
+import java.util.Random;
+
 /**
  * Created by brendancunnie on 1/24/16.
  */
@@ -9,9 +11,14 @@ public class AudioRecordEmulator extends AudioRecordAbstract {
     static final String TAG = "AudioRecordEmulator";
     private Thread notifier;
     private AudioEventProcessor audioEventProcessor;
+    private short[] mRandomAudioData = new short[AudioEventProcessor.NUM_FRAMES];
 
     public AudioRecordEmulator(int recorderAudioSource, int recorderSampleRateInHz, int recorderChannelConfig, int recorderAudioFormat, int recorderBufferSizeInBytes) {
-
+        // We create a random buffer of sound to emulate the real AudioRecord
+        Random random = new Random();
+        for (int i = 0; i < mRandomAudioData.length; i++) {
+            mRandomAudioData[i] = (short) (random.nextInt(Short.MAX_VALUE * 2) - Short.MAX_VALUE);
+        }
     }
 
     @Override
@@ -38,8 +45,9 @@ public class AudioRecordEmulator extends AudioRecordAbstract {
             public void run() {
                 while (true) {
                     try {
-                        Thread.currentThread().sleep(200);
-                        audioEventProcessor.onPeriodicNotificationEmulator();
+                        Thread.currentThread();
+                        sleep(1000 / AudioEventProcessor.UPDATES_PER_SECOND);
+                        audioEventProcessor.onPeriodicNotification(mRandomAudioData);
                     } catch (InterruptedException e) {
                         Log.i(TAG, "run()   Huh.  InterruptedException thrown while sleep()ing.");
                         e.printStackTrace();
