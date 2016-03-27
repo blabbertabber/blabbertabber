64ea26fcb830fdab72db00025d36e8d6e6c6903c
Brendan Cunnie <saintbrendan@gmail.com>
2016-03-27 07:48:16 -0700

    AcknowledgementsActivity -> AboutActivity
    Lay the groundword for "About" selection which will show
    version number as well as acknowledgements.
    
    Signed-off-by: Brian Cunnie <brian.cunnie@gmail.com>

--- app/src/main/java/com/blabbertabber/blabbertabber/AboutActivity.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/AboutActivity.java
@@ -4,13 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
-public class AcknowledgementsActivity extends Activity {
+public class AboutActivity extends Activity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_acknowledgements);
+        setContentView(R.layout.activity_about);
     }
 
     public void finish(View v) {
