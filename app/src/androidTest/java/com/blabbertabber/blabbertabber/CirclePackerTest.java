2c14e0ababb7ed9c636b645b3dadb532aeae99a8
Brian Cunnie <brian.cunnie@gmail.com>
2016-04-23 10:51:27 -0700

    CirclePacker WIP
    Signed-off-by: Brendan Cunnie <saintbrendan@gmail.com>

--- app/src/androidTest/java/com/blabbertabber/blabbertabber/CirclePackerTest.java
+++ app/src/androidTest/java/com/blabbertabber/blabbertabber/CirclePackerTest.java
@@ -5,6 +5,8 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.security.InvalidParameterException;
+
 import math.geom2d.conic.Circle2D;
 
 import static org.junit.Assert.assertEquals;
@@ -19,9 +21,42 @@ public class CirclePackerTest {
     @Test
     public void packsOneCircleAsLargeAsPossible() {
         CirclePacker cp = new CirclePacker(72, 72);
-        Circle2D[] circles = cp.addCircle(new double[]{10});
+        Circle2D[] circles = cp.addCircles(new double[]{10});
         assertEquals("The X coordinate should be 36", 36.0, circles[0].center().getX(), 0.1);
         assertEquals("The Y coordinate should be 36", 36.0, circles[0].center().getY(), 0.1);
         assertEquals("The radius should be 36", 36.0, circles[0].radius(), 0.1);
+    }
+
+    @Test
+    public void packsTwoEqualCircles() {
+        CirclePacker cp = new CirclePacker(4, 2);
+        Circle2D[] circles = cp.addCircles(new double[]{1, 1});
+
+        assertEquals("The first circle's X coordinate should be 1", 1.0, circles[0].center().getX(), 0.1);
+        assertEquals("The first circle's Y coordinate should be 1", 1.0, circles[0].center().getY(), 0.1);
+
+        assertEquals("The second circle's X coordinate should be 3", 3.0, circles[1].center().getX(), 0.1);
+        assertEquals("The second circle's Y coordinate should be 1", 1.0, circles[1].center().getY(), 0.1);
+    }
+
+    @Test(expected=InvalidParameterException.class)
+    public void packsTwoTooBigCircles() {
+        CirclePacker cp = new CirclePacker(4, 2);
+        cp.addCircles(new double[]{3, 2});
+    }
+
+    @Test
+    public void packsThreeEqualCircles() {
+        CirclePacker cp = new CirclePacker(6, 2);
+        Circle2D[] circles = cp.addCircles(new double[]{1, 1, 1});
+
+        assertEquals("The first circle's X coordinate should be 1", 1.0, circles[0].center().getX(), 0.1);
+        assertEquals("The first circle's Y coordinate should be 1", 1.0, circles[0].center().getY(), 0.1);
+
+        assertEquals("The second circle's X coordinate should be 3", 3.0, circles[1].center().getX(), 0.1);
+        assertEquals("The second circle's Y coordinate should be 1", 1.0, circles[1].center().getY(), 0.1);
+
+        assertEquals("The third circle's X coordinate should be 2", 2.0, circles[1].center().getX(), 0.1);
+        assertEquals("The third circle's Y coordinate should be 1", 1.0, circles[1].center().getY(), 0.1);
     }
 }
