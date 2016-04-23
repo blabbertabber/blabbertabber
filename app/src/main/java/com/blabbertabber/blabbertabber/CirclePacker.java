5853316cabf5a565ea42237c9f85aaf28c84a24f
Brian Cunnie <brian.cunnie@gmail.com>
2016-04-23 10:51:27 -0700

    CirclePacker WIP
    Signed-off-by: Brendan Cunnie <saintbrendan@gmail.com>

--- app/src/main/java/com/blabbertabber/blabbertabber/CirclePacker.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/CirclePacker.java
@@ -1,5 +1,9 @@
 package com.blabbertabber.blabbertabber;
 
+import java.security.InvalidParameterException;
+import java.util.HashSet;
+
+import math.geom2d.Point2D;
 import math.geom2d.conic.Circle2D;
 
 /**
@@ -14,10 +18,48 @@ public class CirclePacker {
         this.y = y;
     }
 
-    public Circle2D[] addCircle(double[] radii) {
+    /**
+     * Place the circles within the rectangle.
+     *
+     * @param radii
+     * @return The array of circles packed into the rectangle.
+     * @assumption The radii will be in descending order.
+     */
+    public Circle2D[] addCircles(double[] radii) {
         if (radii.length == 1) {
             double radius = (x > y ? x : y) / 2;
             return new Circle2D[]{new Circle2D(radius, radius, radius)};
+        } else if (radii.length == 2) {
+            Circle2D circle1 = new Circle2D(radii[0], radii[0], radii[0]);
+            Circle2D circle2 = new Circle2D(x - radii[1], radii[1], radii[1]);
+            Point2D center1 = circle1.center();
+            Point2D center2 = circle2.center();
+            double distance = center1.distance(center2.x(), center2.y());
+            double minDistance = radii[0] + radii[1];
+            if (distance < minDistance) {
+                String msg = "The radii must be at least " + minDistance + " apart.  " +
+                        "But they are only " + distance + " apart.";
+                throw new InvalidParameterException(msg);
+            }
+            return new Circle2D[]{circle1, circle2};
+        } else if (radii.length == 3) {
+            // At first 4 corners
+            Circle2D circle1 = new Circle2D(radii[0], radii[0], radii[0]);
+            // with each circle, 4 new corners
+            // circle1 has 2 new corners along the bottom, and 2 new corners along the left edge.
+            // Is it easier to track corners?  Or contact points, each of which is 2 corners?
+
+            HashSet<Double> remainingRadii = new HashSet<>(radii.length);
+            for (int i = 1; i < radii.length; i++) {
+                remainingRadii.add(radii[i]);
+            }
+            for (double radius: remainingRadii) {
+                Circle2D circle2 = new Circle2D(x - radius, radius, radius);
+                /// pack the third circle
+
+            }
+
+
         }
         return null;
     }
