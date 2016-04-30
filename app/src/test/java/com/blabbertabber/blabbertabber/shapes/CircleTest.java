f087c4c05d20bf0e99be6438f61f4b6aa54fab5b
Brendan Cunnie <saintbrendan@gmail.com>
2016-04-30 09:31:39 -0700

    Shape classes have thorough test coverage
    Signed-off-by: Brian Cunnie <brian.cunnie@gmail.com>

--- app/src/test/java/com/blabbertabber/blabbertabber/shapes/CircleTest.java
+++ app/src/test/java/com/blabbertabber/blabbertabber/shapes/CircleTest.java
@@ -11,6 +11,7 @@ import java.util.Collection;
 
 import math.geom2d.Point2D;
 import math.geom2d.conic.Circle2D;
+import math.geom2d.line.Line2D;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -47,5 +48,21 @@ public class CircleTest {
         Shape secondCircle = Shape.makeCircle(new Circle2D(new Point2D(2, 0), 0.99));
         Collection<Point2D> points = firstCircle.intersections(secondCircle);
         assertEquals("The lines don't intersect", 0, points.size());
+    }
+
+    @Test
+    public void testCirclesIntersectLines() {
+        Shape circle = Shape.makeCircle(new Circle2D(new Point2D(0, 0), 1));
+        Shape line = Shape.makeLine(new Line2D(new Point2D(-3, 0), new Point2D(3, 0)));
+        Collection<Point2D> points = circle.intersections(line);
+        assertEquals("There are exactly two points of intersection", 2, points.size());
+        double sumX = 0;
+        for (Point2D point : points) {
+            sumX += point.x();
+            assertEquals("The x-intersection is at 1", 1, Math.abs(point.x()), 0.000000001);
+            assertEquals("The y-intersection is at 0", 0, point.y(), 0.000000001);
+        }
+        // the above test will pass if the X points are BOTH 1; this test makes sure that their signs oppose
+        assertEquals("The sum of X points should be 0", 0, sumX, 0.0000000001);
     }
 }
