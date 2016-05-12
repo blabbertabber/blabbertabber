1bf0857770e455d712888a5dbd15dbaebb3efda6
Brendan Cunnie <saintbrendan@gmail.com>
2016-05-11 18:59:19 -0700

    New Summary Activity displays Packed Circles
    - FIXME: including the image in the PackedCircleActivity's layout
      causes 7 of the tests to fail. We need to fix this & include
      the image in the layout
    - currently a mock-up
    
    Signed-off-by: Brian Cunnie <brian.cunnie@gmail.com>
    Signed-off-by: Brendan Cunnie <saintbrendan@gmail.com>

---
+++ app/src/main/java/com/blabbertabber/blabbertabber/PackedCircleActivity.java
@@ -0,0 +1,13 @@
+package com.blabbertabber.blabbertabber;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PackedCircleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_packed_circle);
+    }
+}
