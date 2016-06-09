f3ed53a6df7b2a4aa5496c663c03373f7765c0d2
Brendan Cunnie <saintbrendan@gmail.com>
2016-06-08 23:41:47 -0400

    Enbiggened circles.  Corrected height/width.
    Signed-off-by: Brian Cunnie <brian.cunnie@gmail.com>

--- app/src/main/java/com/blabbertabber/blabbertabber/CircleView.java
+++ app/src/main/java/com/blabbertabber/blabbertabber/CircleView.java
@@ -54,16 +54,44 @@ public class CircleView extends View {
         paint.setColor(Color.DKGRAY);
         paint.setStrokeWidth(1);
         // FIXME: move this OUT of onDraw ASAP
-        Box2D box = new Box2D(0, canvas.getHeight(), 0, canvas.getWidth());
+        Box2D box = new Box2D(0, canvas.getWidth(), 0, canvas.getHeight());
+        float x = canvas.getWidth() - 1;
+        float y = canvas.getHeight() - 1;
+        float[] pts = {
+                0, 0, x, y,
+                x, y, x, 0,
+                x, 0, 0, y,
+                0, 0, x, 0,
+                0, 0, 0, y,
+                x, y, 0, y,
+                0, y, x, y};
+
+        canvas.drawLines(pts, paint);
         ContainingRectangle rect = new ContainingRectangle(box);
-        List<Double> radiusList = Arrays.asList(new Double[]{200.0, 100.0, 30.0});
-        ArrayList radii = new ArrayList(radiusList);
-        Collection<Stack<Circle2D>> solutions = rect.placeFirstCircle(radii);
+        Double[] originalRadii = {200.0, 100.0, 30.0};
+        Double[] radii = enbiggenRadii(box, originalRadii);
+        List<Double> radiusList = Arrays.asList(radii);
+        ArrayList radiiArrayList = new ArrayList(radiusList);
+        Collection<Stack<Circle2D>> solutions = rect.placeFirstCircle(radiiArrayList);
         Stack<Circle2D> circles = (Stack<Circle2D>) solutions.toArray()[0];
         for (Circle2D c : circles) {
             Log.i(TAG, "c.radius(): " + c.radius());
             canvas.drawCircle((float) c.center().x(), (float) c.center().y(), (float) c.radius(), paint);
         }
+    }
+
+    private Double[] enbiggenRadii(Box2D box, Double[] radii) {
+        double boxArea = box.getHeight() * box.getWidth();
+        double circlesArea = 0;
+        for (double radius : radii) {
+            circlesArea += Math.PI * radius * radius;
+        }
+        double ratio = (boxArea / circlesArea) * 1 / 5;
+        Double[] newRadii = new Double[radii.length];
+        for (int i = 0; i < radii.length; i++) {
+            newRadii[i] = radii[i] * ratio;
+        }
+        return newRadii;
     }
 
 }
